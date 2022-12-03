0027a82884ab91b1f8fd30b2ca93bec7b3dd33b3
Ashok Lama <moktashok@gmail.com>
2022-12-03 18:38:39 +0545

    Article UI and See All Button and sme fixes

--- src/Pages/Articles.js
+++ src/Pages/Articles.js
@@ -2,22 +2,84 @@
 import React from 'react'
 import styled from 'styled-components'
 import ArticleItem from '../components/articleItem'
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+import { Icon } from '@iconify/react'
 
 // importing the react components
 import Page from '../components/page'
+import { responsive } from '../service/responsive'
+
+// importing testing component
+import ArtcileThumbnail from '../Images/registration.jpg'
+import { Link } from 'react-router-dom'
 
 // styled components
-const ArticleWrapper = styled.section`
-  & > h2 {
-    font-size: 1.1rem;
-    color: var(--text-black);
-  }
+const ArticleWrapper = styled.div`
+  width: 100%;
+  overflow-x: auto;
 
   & > div {
-    max-width: 950px;
-    display: grid;
-    grid-template-columns: 30% 30% 30%;
-    grid-gap: 5%;
+    margin-bottom: 1rem;
+
+    & > section {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      flex-wrap: wrap;
+
+      & > h2 {
+        font-size: 1.1rem;
+        color: var(--text-black);
+        margin-bottom: 1rem;
+        position: relative;
+        width: fit-content;
+        text-transform: capitalize;
+        cursor: pointer;
+
+        ::before {
+          position: absolute;
+          content: '';
+          width: 40%;
+          height: 5px;
+          transition: 0.19s ease-in-out;
+          border-radius: 10px;
+          background-color: var(--text-blue);
+          top: 100%;
+          left: 0;
+        }
+
+        :hover {
+          ::before {
+            width: 90%;
+          }
+        }
+      }
+
+      & > a {
+        display: flex;
+        text-decoration: none;
+        color: var(--text-light-black);
+        align-items: center;
+        margin-right: 3.5rem;
+        font-weight: bold;
+        font-size: 0.9rem;
+
+        :hover {
+          color: var(--text-blue);
+        }
+
+        & > svg {
+          font-size: 1.3rem;
+          margin-left: 0.1rem;
+        }
+      }
+    }
+
+    .carouselItem {
+      display: block;
+      padding-right: 1rem;
+    }
   }
 `
 
@@ -25,10 +87,115 @@ export default function Article() {
   return (
     <Page title="Articles">
       <ArticleWrapper>
-        <h2>Articles</h2>
-
         <div>
-          <ArticleItem />
+          <section>
+            <h2>Popular</h2>
+            <Link to={'popular'}>
+              See All <Icon icon="material-symbols:arrow-right-alt-rounded" />
+            </Link>
+          </section>
+
+          <Carousel
+            containerClass="carousel-container"
+            responsive={responsive}
+            swipeable={true}
+            draggable={true}
+            itemClass="carouselItem"
+            partialVisible={false}
+            minimumTouchDrag={20}
+          >
+            <ArticleItem
+              thumbnail={ArtcileThumbnail}
+              articleName="Article Name"
+              articleDescription=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo pariatur quod error sint, soluta non, nostrum
+  eos, ipsam nam numquam facilis atque? Accusamus placeat inventore nemo eum sunt temporibus ipsam ab, fuga
+  error voluptates fugit recusandae enim consequuntur quasi deserunt quo laboriosam odio excepturi tenetur
+  nostrum sequi! Expedita, alias."
+              destination="/404"
+              author="Ashok Lama"
+              date="20th August 2022"
+            />
+            <ArticleItem
+              thumbnail={ArtcileThumbnail}
+              articleName="Article Name"
+              articleDescription=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo pariatur quod error sint, soluta non, nostrum
+  eos, ipsam nam numquam facilis atque? Accusamus placeat inventore nemo eum sunt temporibus ipsam ab, fuga
+  error voluptates fugit recusandae enim consequuntur quasi deserunt quo laboriosam odio excepturi tenetur
+  nostrum sequi! Expedita, alias."
+              destination="/404"
+              author="Ashok Lama"
+              date="20th August 2022"
+            />
+            <ArticleItem
+              thumbnail={ArtcileThumbnail}
+              articleName="Article Name"
+              articleDescription=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo pariatur quod error sint, soluta non, nostrum
+  eos, ipsam nam numquam facilis atque? Accusamus placeat inventore nemo eum sunt temporibus ipsam ab, fuga
+  error voluptates fugit recusandae enim consequuntur quasi deserunt quo laboriosam odio excepturi tenetur
+  nostrum sequi! Expedita, alias."
+              destination="/404"
+              author="Ashok Lama"
+              date="20th August 2022"
+            />
+            <ArticleItem
+              thumbnail={ArtcileThumbnail}
+              articleName="Article Name"
+              articleDescription=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo pariatur quod error sint, soluta non, nostrum
+  eos, ipsam nam numquam facilis atque? Accusamus placeat inventore nemo eum sunt temporibus ipsam ab, fuga
+  error voluptates fugit recusandae enim consequuntur quasi deserunt quo laboriosam odio excepturi tenetur
+  nostrum sequi! Expedita, alias."
+              destination="/404"
+              author="Ashok Lama"
+              date="20th August 2022"
+            />
+            <ArticleItem
+              thumbnail={ArtcileThumbnail}
+              articleName="Article Name"
+              articleDescription=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo pariatur quod error sint, soluta non, nostrum
+  eos, ipsam nam numquam facilis atque? Accusamus placeat inventore nemo eum sunt temporibus ipsam ab, fuga
+  error voluptates fugit recusandae enim consequuntur quasi deserunt quo laboriosam odio excepturi tenetur
+  nostrum sequi! Expedita, alias."
+              destination="/404"
+              author="Ashok Lama"
+              date="20th August 2022"
+            />
+            <ArticleItem
+              thumbnail={ArtcileThumbnail}
+              articleName="Article Name"
+              articleDescription=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo pariatur quod error sint, soluta non, nostrum
+  eos, ipsam nam numquam facilis atque? Accusamus placeat inventore nemo eum sunt temporibus ipsam ab, fuga
+  error voluptates fugit recusandae enim consequuntur quasi deserunt quo laboriosam odio excepturi tenetur
+  nostrum sequi! Expedita, alias."
+              destination="/404"
+              author="Ashok Lama"
+              date="20th August 2022"
+            />
+          </Carousel>
+        </div>
+        <div>
+          <section>
+            <h2>Latest</h2>
+            <Link to={'latest'}>
+              See All <Icon icon="material-symbols:arrow-right-alt-rounded" />
+            </Link>
+          </section>
+
+          <Carousel
+            containerClass="carousel-container"
+            responsive={responsive}
+            swipeable={true}
+            draggable={true}
+            itemClass="carouselItem"
+            partialVisible={false}
+            minimumTouchDrag={20}
+          >
+            <ArticleItem />
+            <ArticleItem />
+            <ArticleItem />
+            <ArticleItem />
+            <ArticleItem />
+            <ArticleItem />
+          </Carousel>
         </div>
       </ArticleWrapper>
     </Page>
--- src/components/articleItem.js
+++ src/components/articleItem.js
@@ -2,18 +2,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
 // importing components
 import ArtcileThumbnail from '../Images/registration.jpg'
 
 // styled components
 const Article = styled.section`
-  width: 100%;
   overflow: hidden;
   background-color: var(--background-white);
   border: 0.15rem solid var(--light-border-color);
-  border-radius: 0.19rem;
-  
+  border-radius: 0.25rem;
+
   & > div {
     width: 100%;
     padding-top: 75%;
@@ -31,7 +31,61 @@ const Article = styled.section`
   }
 
   & section {
-    padding: 1rem;
+    padding: 0.5rem;
+
+    & > h3 {
+      font-size: 0.95rem;
+      color: var(--text-black);
+      line-height: 1.5;
+      margin-bottom: 0.45rem;
+    }
+
+    & > p {
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      -webkit-line-clamp: 3;
+    }
+
+    & > p,
+    & > div span {
+      font-size: 0.85rem;
+      color: var(--text-light-black);
+      line-height: 1.5;
+    }
+
+    & > a {
+      text-decoration: none;
+      color: var(--text-blue);
+      font-weight: bold;
+      margin: 1.15rem 0;
+      display: block;
+      font-size: 0.85rem;
+      width: fit-content;
+
+      :hover {
+        color: var(--hover-404-blue);
+        text-decoration: underline;
+      }
+    }
+
+    & > div {
+      display: grid;
+      grid-template-columns: 50% 50%;
+      margin: -0.5rem;
+      margin-top: 0.9rem;
+      border-top: 0.15rem solid var(--light-border-color);
+
+      & > span {
+        padding: 0.5rem;
+        text-transform: capitalize;
+
+        :first-child {
+          border-right: 0.15rem solid var(--light-border-color);
+        }
+      }
+    }
   }
 `
 
@@ -39,19 +93,37 @@ export default function ArticleItem(props) {
   return (
     <Article>
       <div>
-        <img src={ArtcileThumbnail} alt="Thumbnail name" />
+        <img src={props.thumbnail} alt={props.articleName} />
       </div>
 
       <section>
-        <h3>Article Name</h3>
-        <p>article brief</p>
+        <h3>{props.articleName}</h3>
+        <p>{props.articleDescription}</p>
         <Link to={props.destination}>Read full article ...</Link>
 
         <div>
-          <span>By: Ashok Lama</span>
-          <span>On: 6th October 2019</span>
+          <span>By: {props.author}</span>
+          <span>On: {props.date}</span>
         </div>
       </section>
     </Article>
   )
+}
+
+ArticleItem.propTypes = {
+  thumbnail: PropTypes.string,
+  articleName: PropTypes.string,
+  articleDescription: PropTypes.string,
+  destination: PropTypes.string,
+  author: PropTypes.string,
+  date: PropTypes.string,
+}
+
+ArticleItem.defaultProps = {
+  thumbnail: ArtcileThumbnail,
+  articleName: 'Unknown',
+  articleDescription: 'N/A',
+  destination: '/404',
+  author: 'Unknown',
+  date: 'unknown',
 }
